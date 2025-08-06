42026c45f36a43a1131fa13ccf6b4831de9bf0fa
admin2-3sigma <admin2@3sigma-studios.com>
2025-08-06 11:06:33 +0200

    English test done

--- i18n/hu_HU.ts
+++ i18n/hu_HU.ts
@@ -4,174 +4,212 @@
 <context>
     <name>HomePage</name>
     <message>
+        <location filename="../qml/HomePage.qml" line="7"/>
         <source>Main Page</source>
-        <translation type="vanished">Főoldal</translation>
+        <translation>Főoldal</translation>
     </message>
     <message>
+        <location filename="../qml/HomePage.qml" line="19"/>
         <source>Please write your name.</source>
-        <translation type="vanished">Kérlek, írd be a neved.</translation>
+        <translation>Kérlek, írd be a neved.</translation>
     </message>
     <message>
+        <location filename="../qml/HomePage.qml" line="40"/>
         <source>Start</source>
-        <translation type="vanished">Indítás</translation>
+        <translation>Indítás</translation>
     </message>
     <message>
+        <location filename="../qml/HomePage.qml" line="58"/>
         <source>Switch to en_EN</source>
-        <translation type="vanished">Váltás en_EN-re</translation>
+        <translation>Váltás en_EN-re</translation>
     </message>
     <message>
+        <location filename="../qml/HomePage.qml" line="67"/>
         <source>Switch to de_DE</source>
-        <translation type="vanished">Váltás de_DE-re</translation>
+        <translation>Váltás de_DE-re</translation>
     </message>
     <message>
+        <location filename="../qml/HomePage.qml" line="76"/>
         <source>Switch to de_AT</source>
-        <translation type="vanished">Váltás de_AT-ra</translation>
+        <translation>Váltás de_AT-ra</translation>
     </message>
     <message>
+        <location filename="../qml/HomePage.qml" line="85"/>
         <source>Switch to fr_FR</source>
-        <translation type="vanished">Váltás fr_FR-re</translation>
+        <translation>Váltás fr_FR-re</translation>
     </message>
     <message>
+        <location filename="../qml/HomePage.qml" line="94"/>
         <source>Switch to cn_CN</source>
-        <translation type="vanished">Váltás cn_CN-re</translation>
+        <translation>Váltás cn_CN-re</translation>
     </message>
     <message>
+        <location filename="../qml/HomePage.qml" line="103"/>
         <source>Switch to hu_HU</source>
-        <translation type="vanished">Váltás hu_HU-ra</translation>
+        <translation>Váltás hu_HU-ra</translation>
     </message>
     <message>
+        <location filename="../qml/HomePage.qml" line="137"/>
         <source>Thank you, the  test is finished.</source>
-        <translation type="vanished">Köszönjük, a teszt befejeződött.</translation>
+        <translation>Köszönjük, a teszt befejeződött.</translation>
     </message>
 </context>
 <context>
     <name>Main</name>
     <message>
+        <location filename="../build/Felgo_SDK_Desktop_Qt_6_4_1_MinGW_64_bit-Debug/qml/Main.qml" line="15"/>
         <source>Main Page</source>
-        <translation type="vanished">Főoldal</translation>
+        <translation>Főoldal</translation>
     </message>
 </context>
 <context>
     <name>MultipeChoiceQuestion</name>
     <message>
+        <location filename="../qml/MultipeChoiceQuestion.qml" line="62"/>
         <source>Next</source>
-        <translation type="vanished">Következő</translation>
+        <translation>Következő</translation>
     </message>
 </context>
 <context>
     <name>MultipleChoice</name>
     <message>
+        <location filename="../qml/MultipleChoice.qml" line="15"/>
         <source>All birds have feathers. A penguin is a bird.</source>
-        <translation type="vanished">Minden madárnak vannak tollai. A pingvin egy madár.</translation>
+        <translation>Minden madárnak vannak tollai. A pingvin egy madár.</translation>
     </message>
     <message>
+        <location filename="../qml/MultipleChoice.qml" line="16"/>
         <source>If an animal has feathers, then it is a bird.</source>
-        <translation type="vanished">Ha egy állaton tollak vannak, akkor az egy madár.</translation>
+        <translation>Ha egy állaton tollak vannak, akkor az egy madár.</translation>
     </message>
     <message>
+        <location filename="../qml/MultipleChoice.qml" line="17"/>
         <source>If something is a bird, then it has feathers.</source>
-        <translation type="vanished">Ha valami madár, akkor tollai vannak.</translation>
+        <translation>Ha valami madár, akkor tollai vannak.</translation>
     </message>
     <message>
+        <location filename="../qml/MultipleChoice.qml" line="18"/>
         <source>Only birds have feathers.</source>
-        <translation type="vanished">Csak a madaraknak vannak tollai.</translation>
+        <translation>Csak a madaraknak vannak tollai.</translation>
     </message>
     <message>
+        <location filename="../qml/MultipleChoice.qml" line="19"/>
         <source>Pinguin have feathers</source>
-        <translation type="vanished">A pingvineknek vannak tollai</translation>
+        <translation>A pingvineknek vannak tollai</translation>
     </message>
     <message>
+        <location filename="../qml/MultipleChoice.qml" line="26"/>
         <source>Everyone who graduates from medical school is a doctor.</source>
-        <translation type="vanished">Minden, aki elvégzi az orvosi egyetemet, orvos lesz.</translation>
+        <translation>Minden, aki elvégzi az orvosi egyetemet, orvos lesz.</translation>
     </message>
     <message>
+        <location filename="../qml/MultipleChoice.qml" line="27"/>
         <source>Everyone who is a doctor graduated from medical school.</source>
-        <translation type="vanished">Minden orvos elvégezte az orvosi egyetemet.</translation>
+        <translation>Minden orvos elvégezte az orvosi egyetemet.</translation>
     </message>
     <message>
+        <location filename="../qml/MultipleChoice.qml" line="28"/>
         <source>Some doctors didn’t go to medical school.</source>
-        <translation type="vanished">Vannak orvosok, akik nem jártak orvosi egyetemre.</translation>
+        <translation>Vannak orvosok, akik nem jártak orvosi egyetemre.</translation>
     </message>
     <message>
+        <location filename="../qml/MultipleChoice.qml" line="29"/>
         <source>Only doctors go to medical school</source>
-        <translation type="vanished">Csak orvosok járnak orvosi egyetemre</translation>
+        <translation>Csak orvosok járnak orvosi egyetemre</translation>
     </message>
     <message>
+        <location filename="../qml/MultipleChoice.qml" line="30"/>
         <source>If somebody is not a doctor, then he did not graduate from medical school</source>
-        <translation type="vanished">Ha valaki nem orvos, akkor nem végzett el orvosi egyetemet</translation>
+        <translation>Ha valaki nem orvos, akkor nem végzett el orvosi egyetemet</translation>
     </message>
     <message>
+        <location filename="../qml/MultipleChoice.qml" line="37"/>
         <source>If it is raining, then the ground is wet.</source>
-        <translation type="vanished">Ha esik az eső, akkor a föld nedves.</translation>
+        <translation>Ha esik az eső, akkor a föld nedves.</translation>
     </message>
     <message>
+        <location filename="../qml/MultipleChoice.qml" line="38"/>
         <source>If the ground is wet, then it is raining</source>
-        <translation type="vanished">Ha a föld nedves, akkor esik az eső</translation>
+        <translation>Ha a föld nedves, akkor esik az eső</translation>
     </message>
     <message>
+        <location filename="../qml/MultipleChoice.qml" line="39"/>
         <source>If the ground is not wet, then it is not raining.</source>
-        <translation type="vanished">Ha a föld nem nedves, akkor nem esik az eső.</translation>
+        <translation>Ha a föld nem nedves, akkor nem esik az eső.</translation>
     </message>
     <message>
+        <location filename="../qml/MultipleChoice.qml" line="40"/>
         <source>If it is not raining, then the ground is not wet.</source>
-        <translation type="vanished">Ha nem esik az eső, akkor a föld nem nedves.</translation>
+        <translation>Ha nem esik az eső, akkor a föld nem nedves.</translation>
     </message>
     <message>
+        <location filename="../qml/MultipleChoice.qml" line="41"/>
         <source>It is possible that the ground is wet even if it is not raining</source>
-        <translation type="vanished">Lehetséges, hogy a föld nedves, még ha nem is esik eső</translation>
+        <translation>Lehetséges, hogy a föld nedves, még ha nem is esik eső</translation>
     </message>
     <message>
+        <location filename="../qml/MultipleChoice.qml" line="48"/>
         <source>If a card has a vowel on it, then it has an even number on the other side. You see these four cards: A | K | 4 | 7. Which card must you turn to test if the rule is true?</source>
-        <translation type="vanished">Ha egy kártyán magánhangzó van, akkor a másik oldalán páros szám található. Ezeket a négy kártyát látod: A | K | 4 | 7. Melyiket kell megfordítani a szabály teszteléséhez?</translation>
+        <translation>Ha egy kártyán magánhangzó van, akkor a másik oldalán páros szám található. Ezeket a négy kártyát látod: A | K | 4 | 7. Melyiket kell megfordítani a szabály teszteléséhez?</translation>
     </message>
     <message>
+        <location filename="../qml/MultipleChoice.qml" line="49"/>
         <source>A</source>
-        <translation type="vanished">A</translation>
+        <translation>A</translation>
     </message>
     <message>
+        <location filename="../qml/MultipleChoice.qml" line="50"/>
         <source>K</source>
-        <translation type="vanished">K</translation>
+        <translation>K</translation>
     </message>
     <message>
+        <location filename="../qml/MultipleChoice.qml" line="51"/>
         <source>4</source>
-        <translation type="vanished">4</translation>
+        <translation>4</translation>
     </message>
     <message>
+        <location filename="../qml/MultipleChoice.qml" line="52"/>
         <source>7</source>
-        <translation type="vanished">7</translation>
+        <translation>7</translation>
     </message>
 </context>
 <context>
     <name>SeriesTestQuestion</name>
     <message>
+        <location filename="../qml/SeriesTestQuestion.qml" line="8"/>
         <source>What comes next: </source>
-        <translation type="vanished">Folytasd a sororatot: </translation>
+        <translation>Folytasd a sororatot: </translation>
     </message>
     <message>
+        <location filename="../qml/SeriesTestQuestion.qml" line="70"/>
         <source>Next</source>
-        <translation type="vanished">Következő</translation>
+        <translation>Következő</translation>
     </message>
 </context>
 <context>
     <name>TestCreativity</name>
     <message>
+        <location filename="../qml/TestCreativity.qml" line="12"/>
         <source>Name as many uses as you can for a brick.</source>
-        <translation type="vanished">Sorold fel minél többféle felhasználási módját egy téglának.</translation>
+        <translation>Sorold fel minél többféle felhasználási módját egy téglának.</translation>
     </message>
     <message>
+        <location filename="../qml/TestCreativity.qml" line="18"/>
         <source>Think of as many unusual uses as possible for a shoe.</source>
-        <translation type="vanished">Gondolj ki minél több szokatlan használati módját egy cipőnek.</translation>
+        <translation>Gondolj ki minél több szokatlan használati módját egy cipőnek.</translation>
     </message>
     <message>
+        <location filename="../qml/TestCreativity.qml" line="24"/>
         <source>Think of as many unusual uses as possible for a plastic bottle.</source>
-        <translation type="vanished">Találj ki minél több szokatlan használati módját egy műanyag palacknak.</translation>
+        <translation>Találj ki minél több szokatlan használati módját egy műanyag palacknak.</translation>
     </message>
 </context>
 <context>
     <name>TestCreativityQuestions</name>
     <message>
+        <location filename="../qml/TestCreativityQuestions.qml" line="75"/>
         <source>Next</source>
-        <translation type="vanished">Következő</translation>
+        <translation>Következő</translation>
     </message>
 </context>
 </TS>
